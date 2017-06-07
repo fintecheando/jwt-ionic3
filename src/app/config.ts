7f5e9b07152139ddc89ae6548fd3650d103ed691
Victor Manuel Romero Rodriguez <victorromero@afiliamedica.com.mx>
2017-06-06 20:22:45 -0500

    commit

---
+++ src/app/config.ts
@@ -0,0 +1,10 @@
+export let cfg = {
+  authUrl: 'http://afiliamedicatech1.bitnamiapp.com:1780/api',
+  apiUrl: 'http://afiliamedicatech1.bitnamiapp.com:1780/api',
+  tokenName: 'token',
+  user: {
+    register: '/auth/signup',
+    login: '/auth/login'
+  },
+  books: '/books'
+};
