1be67278303028434df1a7c3251470aa18f2373f
Victor Manuel Romero Rodriguez <victorromero@afiliamedica.com.mx>
2017-06-27 13:00:39 -0500

    Se integra el login2

--- src/pages/register-page/register-page.ts
+++ src/pages/register-page/register-page.ts
@@ -41,70 +41,82 @@ export class RegisterPage {
 
   }
 
-  ionViewDidLoad() {
-    //hide menu when on the login page, regardless of the screen resolution
-    this.menuCtrl.enable(false);
-    
-    this.tts.speak({
-        locale: 'es-US',
-            rate: 0.9,
-            text: 'Afíliate como Médico'
-        })
-        .then(() => console.log('Success'))
-        .catch((reason: any) => console.log(reason));
-    
-    
-  }
+    ionViewDidLoad() {
+        //hide menu when on the login page, regardless of the screen resolution
+        this.menuCtrl.enable(false);
+        this.tts.speak({
+            locale: 'es-US',
+                rate: 0.9,
+                text: 'Afíliate como Médico'
+            })
+            .then(() => console.log('Success'))
+            .catch((reason: any) => console.log(reason));
+    }
 
-  register() {
-    
-    this.authService.register(this.regData.value)
-      .then(() => this.navCtrl.setRoot('ProfilePage'))
-      .catch(e => console.log("reg error", e));         
-  }
+    register() {
+        this.authService.register(this.regData.value)
+            .then(() => this.navCtrl.setRoot('ProfilePage'))
+            .catch(e => console.log("reg error", e));         
+    }
   
     async hasPermission():Promise<boolean> {
         try {
-          const permission = await this.speech.hasPermission();
-          console.log(permission);
-
-          return permission;
+                const permission = await this.speech.hasPermission();
+                console.log(permission);
+                return permission;
         } catch(e) {
-          console.log(e);
+            console.log(e);
         }
-      }
+    }
 
     async getPermission():Promise<void> {
         try {
-          this.speech.requestPermission();
+            this.speech.requestPermission();
         } catch(e) {
-          console.log(e);
+            console.log(e);
         }
     }
     
-    
-
-    
     listenUserName(): void {
-    console.log('listen action triggered');
-    if (this.isListening) {
-      this.speech.stopListening();
-      this.toggleListenMode();
-      return;
-    }
+        console.log('listen action triggered');
+        if (this.isListening) {
+            this.speech.stopListening();
+            this.toggleListenMode();
+            return;
+        }
 
-    this.toggleListenMode();
+        this.toggleListenMode();
     
-    let _this = this;
+        let _this = this;
 
-    this.speech.startListening()
-      .subscribe(matches => {
-        _this.zone.run(() => {
-          _this.matches = matches;
-          _this.regData.controls['username'].setValue(matches[0]);
-        })
-      }, error => console.error(error));
+        this.speech.startListening()
+            .subscribe(matches => {
+                _this.zone.run(() => {
+                    _this.matches = matches;
+                    _this.regData.controls['username'].setValue(matches[0]);
+                })
+            }, error => console.error(error));
+    }
+    
+    listenEmail(): void {
+        console.log('listen action triggered');
+        if (this.isListening) {
+            this.speech.stopListening();
+            this.toggleListenMode();
+            return;
+        }
 
+        this.toggleListenMode();
+    
+        let _this = this;
+
+        this.speech.startListening()
+            .subscribe(matches => {
+                _this.zone.run(() => {
+                    _this.matches = matches;
+                    _this.regData.controls['email'].setValue(matches[0]);
+                })
+            }, error => console.error(error));
     }
 
     toggleListenMode():void {
